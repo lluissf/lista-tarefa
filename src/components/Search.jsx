70b7a2fb0405afeadea2f4d1ec6fbb25929a88cd
Luis Felipe <luis.felipe.ferreira.da.silva@hotmail.com>
2024-04-22 23:21:18 -0300

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Search = ({ search, setSearch}) => {
+    return (
+        <div>
+          <div className="search">
+            <h2>Pesquisar:</h2>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Digite para pesquisar."
+            />
+          </div>
+        </div>
+      );
+};
+
+export default Search
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,39 @@
+import { useState} from 'react'
+import '../assets/css/form.css'
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !category) return;
+    //adicionar todo
+    addTodo(value, category);
+    //limpar campos
+    setValue("");
+    setCategory("");
+  };
+  return (
+    <div className="todo-form">
+      <h2>Criar Tarefa</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={value}
+          type="text"
+          placeholder="Digite o titulo"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar Tarefa</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm
